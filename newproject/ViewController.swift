5e7f3ede2c0d72061d260d8f2b68b1aa5ba1d18a
Wesley Silva <salvwes@motorola.com>
2020-06-17 20:52:01 -0400

    Initial Commit

---
+++ newproject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newproject
+//
+//  Created by Wesley Salvador on 17/06/20.
+//  Copyright © 2020 Eldorado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
